62c46c2934370fcee926e4471823601d0435fa55
Sina <sarparast@ce.sharif.ir>
2020-07-06 18:26:24 +0430

    improved navigation

--- articles/urls.py
+++ articles/urls.py
@@ -3,9 +3,10 @@ from . import views
 from .views import ArticleListView
 
 urlpatterns = [
-    path('home', ArticleListView.as_view(), name='article-list'),
+
     path('add/', views.AddView.as_view(), name='article-add'),
     path('read/<str:slug>', views.ReadView.as_view(),   name='read_article'),
     path('update/<str:slug>',   views.UpdateView.as_view(), name='update_article'),
-    path('delete/<str:slug>',   views.DeleteView.as_view(), name='delete_article')
+    path('delete/<str:slug>',   views.DeleteView.as_view(), name='delete_article'),
+    path('home', ArticleListView.as_view(), name='article-list')
 ]
