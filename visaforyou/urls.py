4f79ac32777865d872c847b525e9ecfe383cd69c
Sina <sarparast@ce.sharif.ir>
2020-07-06 18:46:19 +0430

    login and logout buttons added

--- visaforyou/urls.py
+++ visaforyou/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
     path('articles/', include('articles.urls')),
     path('admin/', admin.site.urls),
     path(r'^ckeditor/', include('ckeditor_uploader.urls'))
