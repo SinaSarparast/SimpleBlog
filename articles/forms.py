bc85dd03af5f5b91c892ae705cc1a5300d4ee3a9
Sina <sina.sarparast@icloud.com>
2020-07-09 00:29:27 +0430

    Update forms.py

--- articles/forms.py
+++ articles/forms.py
@@ -7,7 +7,6 @@ from ckeditor.widgets import CKEditorWidget
 from ckeditor_uploader.widgets import CKEditorUploadingWidget
 from ckeditor_uploader.fields import RichTextUploadingField
 
-<<<<<<< HEAD
 class NameForm(forms.Form):
     title   =   forms.CharField(label='title', max_length=30)
     category=   forms.ModelChoiceField(
@@ -22,7 +21,7 @@ class NameForm(forms.Form):
     #     empty_label=None
     # )
     # tags    =   models.CharField(max_length=3, choices=LOCATIONS)
-=======
+
 # # class NameForm(forms.Form): (difference between form.form and modelform)
 # Forms created from forms.Form are manually configured by you. You're better off
 # using these for forms that do not directly interact with models. For example
@@ -31,35 +30,25 @@ class NameForm(forms.Form):
 # forms.ModelForm will be automatically created and then can later be
 # tweaked by you. The best examples really are from the superb documentation
 # provided on the Django website.
->>>>>>> 04734b924d6bf86514447a6be59bb3f0387b342f
-
 
 class ArticleForm(ModelForm):
-    content =   forms.CharField(
-<<<<<<< HEAD
+    content =   forms.CharField( 
         widget  =   FroalaEditor,
         label   =   'content'
         )
     category=   forms.ModelChoiceField(
         queryset =   Category.objects.all(),
-        label   =   'category'
-=======
+        label   =   'category',
         widget  = CKEditorUploadingWidget(config_name='default')
-        # label   =   'متن'
         )
     # content = RichTextUploadingField()
     title =   forms.CharField(
         label   =   'Title',
         initial =   _('Article\'s Title')
->>>>>>> 04734b924d6bf86514447a6be59bb3f0387b342f
         )
     class Meta:
         model = Post
         # fields = '__all__'
-
-<<<<<<< HEAD
-        fields = ['title','summary','category','content']
-=======
         fields = ['title','content']
         labels = {
             'summary': 'summary'
@@ -67,7 +56,6 @@ class ArticleForm(ModelForm):
         help_texts = {
             'content': 'content'
         }
->>>>>>> 04734b924d6bf86514447a6be59bb3f0387b342f
         error_messages = {
             'NON_FIELD_ERRORS': {
                 'unique_together': "%(model_name)s's %(field_labels)s are not unique."
