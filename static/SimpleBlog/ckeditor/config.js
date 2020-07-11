185e68745a00c08437e1e1e841423d93d2283462
Sina <sarparast@ce.sharif.ir>
2020-07-11 23:32:01 +0430

    added custome config to ckeditor

--- static/SimpleBlog/ckeditor/config.js
+++ static/SimpleBlog/ckeditor/config.js
@@ -3,10 +3,31 @@
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
+/**
+* use the link below to customize CKeditor toolbar and get the config code
+* https://ckeditor.com/latest/samples/toolbarconfigurator/index.html#advanced
+*/
+
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-  config.extraPlugins = 'autogrow';
 
+  config.toolbar = [
+		{ name: 'document', items: [ 'Source' ] },
+		{ name: 'clipboard', items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
+		{ name: 'editing', items: [ 'Find', 'Replace', '-', 'SelectAll', '-', 'Scayt' ] },
+		'/',
+		{ name: 'basicstyles', items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'CopyFormatting', 'RemoveFormat' ] },
+		{ name: 'paragraph', items: [ 'NumberedList', 'BulletedList', '-', 'Outdent'
+    , 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter',
+     'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl', 'Language' ] },
+		{ name: 'links', items: [ 'Link', 'Unlink', 'Anchor' ] },
+		{ name: 'insert', items: [ 'Image', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar' ] },
+		'/',
+		{ name: 'styles', items: [ 'Styles', 'Format', 'Font', 'FontSize' ] },
+		{ name: 'colors', items: [ 'TextColor', 'BGColor' ] },
+		{ name: 'tools', items: [ 'Maximize', 'ShowBlocks' ] }
+	];
+  config.extraPlugins = 'autogrow';
 };
