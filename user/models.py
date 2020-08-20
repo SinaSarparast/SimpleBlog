30f865126dc925499fdf79bd2b0366252b54f222
Sina <26283630+SinaSarparast@users.noreply.github.com>
2020-08-20 23:01:07 +0430

    added profile class to models

--- user/models.py
+++ user/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.shortcuts import reverse
 
-class Profile(models.Models):
+class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    imge = models.ImageField(defult='default.jpg',upload_to='profile_pics')
+    image = models.ImageField(default='default.jpg',upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def get_absolute_url(self):
+        return reverse('update_profile',kwargs={'pk': self.pk})
